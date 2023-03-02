e301b5483351240faa9c050acf53ff31f6dbb615
shrikantban <shrikantbaan00@gmail.com>
2023-03-02 22:29:42 +0530

    4th commit

--- src/test/java/pageObjects/HomePage.java
+++ src/test/java/pageObjects/HomePage.java
@@ -3,8 +3,7 @@ package pageObjects;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.testng.annotations.Test;
-@Test(groups= {"Master","Sanity","Regression"})
+
 public class HomePage extends BasePage {
 	public HomePage(WebDriver driver) {
 		super(driver);
